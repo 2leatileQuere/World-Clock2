828aa18f1c7819355250d17ebb8986ed409199a6
2leatileQuere <leatilentlhequere@gmail.com>
2024-08-17 21:55:09 +0200

    added moment js and added javascript to my code

--- css/javascript/index.js
+++ css/javascript/index.js
@@ -1,1 +1,25 @@
-alert("Hi there");
+function updateTime() {
+  // Gaborone
+  let GaboroneElemet = document.querySelector("#gaborone");
+  let GaboroneDateElement = GaboroneElemet.querySelector(".date");
+  let GaboroneTimeElement = GaboroneElemet.querySelector(".time");
+  let GaboroneTime = moment().tz("Botswana/Gaborone");
+
+  GaboroneDateElement.innerHTML = GaboroneTime.format("MMMM Do YYYY");
+  GaboroneTimeElement.innerHTML = GaboroneTime.format(
+    "h:mm:ss [<small>]A[/small>]"
+  );
+
+  // Cape Town
+  let CapeTownElemet = document.querySelector("#gaborone");
+  let CapeTownDateElement = GaboroneElemet.querySelector(".date");
+  let CapeTownTimeElement = GaboroneElemet.querySelector(".time");
+  let CapeTownTime = moment().tz("Botswana/Gaborone");
+
+  CapeTownDateElement.innerHTML = GaboroneTime.format("MMMM Do YYYY");
+  CapeTownTimeElement.innerHTML = GaboroneTime.format(
+    "h:mm:ss [<small>]A[/small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
